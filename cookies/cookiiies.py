c0fcbefc1222e9241669e0a5758f02308bbe8d14
Lucas Wagner <lucas.wagner@uni-oldenburg.de>
2021-08-03 15:27:35 +0200

    README update

--- cookies/cookiiies.py
+++ cookies/cookiiies.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import sys
+import datetime
 
 binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
 browser = webdriver.Firefox(firefox_binary=binary)
 
-url = str(sys.argv[1])
-
+domain = str(sys.argv[1])
+url = "https://"+domain+"/"
 browser.get(url)
 browser.implicitly_wait(10000)
-x = input("pls wait")
+x = input("Press any key if you accepted the cookies in the browser \n")
 cookies = browser.get_cookies()
 for cookie in cookies:
-    print(cookie)
+    print("Cookie name --> ", cookie.get('name'))
+    print("Cookie value --> ", cookie.get('value'))
+    print("Cookie domain --> ", cookie.get('domain'))
+    print("Cookie expiry --> ", datetime.datetime.fromtimestamp(cookie.get('expiry')).isoformat())
 
 browser.close()
 
