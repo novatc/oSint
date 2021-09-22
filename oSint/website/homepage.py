f458fc2bf3932be29f7a3e764d6ab1cc1ea50b5e
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2021-09-22 11:21:35 +0200

    frontend for step "accept cookies"

--- oSint/website/homepage.py
+++ oSint/website/homepage.py
@@ -1,18 +1,21 @@
 import json
 from os import path
+import pandas as pd
 
 import validators
 from flask import Blueprint
 from flask import current_app as app
 from flask import flash, redirect, render_template, request, session
 from flask.helpers import url_for
-from oSint.scripts.cookies.cookiiies import get_cookies, scrape_cookies
+from oSint.scripts.cookies.cookiiies import get_cookies, scrape_cookies, start_browser
 from oSint.scripts.cookies.sitemap import scrape_sitemap
 
 homepage = Blueprint('homepage', __name__)
 
+browser = None
+
 @homepage.route('/', methods=['GET', 'POST'])
-def home():
+def step_one():
     if request.method == 'POST':
         
         url = request.form.get('url')
@@ -21,28 +24,57 @@ def home():
         if valid:
 
             base_url = refactor_url(url)
-            urls = scrape_sitemap(base_url)
-            cookies_before, cookies_after = scrape_cookies(base_url, urls)
-            
+            session['url'] = json.dumps(url)
 
-            # # # # # # #
-            # Do stuff! #
-            # # # # # # #
+            sitemap_urls = scrape_sitemap(base_url)
+            session['sitemap'] = json.dumps(sitemap_urls)
 
-            results = {
-                'url' : base_url,
-                'cookies_before' : cookies_before,
-                'cookies_after' : cookies_after
+            global browser 
+            browser = start_browser()
+
+            browser.get(base_url)
+
+            cookies_before = get_cookies(browser, sitemap_urls)
+            cookies = {
+                'cookies_before' : cookies_before
             }
-            session['data'] = json.dumps(results)
-            return redirect(url_for('dashboard.overview'))
+            session['cookies'] = json.dumps(cookies)
+
+
+            browser.get(base_url)
+
+            
+            return redirect(url_for('homepage.step_two'))
         
         else:
             flash("Website not valid.", category='error')
 
 
     return render_template("home.html")
-    
+
+@homepage.route('/accept-cookies', methods=['GET', 'POST'])  
+def step_two():
+    if request.method == 'POST':
+
+        global browser
+        sitemap = json.loads(session['sitemap'])
+        cookies_after = get_cookies(browser, sitemap)
+
+        cookies_after_df = pd.DataFrame(cookies_after)
+
+        browser.close()
+
+        cookies_after = cookies_after_df.to_html()
+        
+        cookies = json.loads(session['cookies'])
+        cookies['cookies_after'] = cookies_after
+            
+        session['cookies'] = json.dumps(cookies)
+
+        return redirect(url_for('dashboard.overview'))
+
+
+    return render_template("accept-cookies.html")
 
 def refactor_url(url):
     return '/'.join(url.split('/')[:3]) 
